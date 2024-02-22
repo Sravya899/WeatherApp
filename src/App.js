e1557b97410e18465bdb320ae1a267d4a34a0a82
Sravya899 <116885841+Sravya899@users.noreply.github.com>
2024-02-22 18:35:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import WeatherApp from './Components/Weather app/Weather app';
+
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+          </div>
+  );
+}
+
+export default App;
